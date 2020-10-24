eb5621d2ad505cf2fd0d3318a5b57ba3e9ad7121
Karl Behrens <karlbehrensg@gmail.com>
2020-10-23 23:48:43 -0300

    Installing PostgreSQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,8 +82,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'hola1234',
     }
 }
 
